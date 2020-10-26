805a9102acc56d4023d881beb9bfb5a179ea56a2
reah350 <h4riis31@gmail.com>
2020-10-26 18:11:36 +0100

    test

--- src/Nastupi.js
+++ src/Nastupi.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import './App.css';
-import { ReactPhotoCollage } from "react-photo-collage";
+import ImageGallery from 'react-image-gallery';
 
 
+const images =
+[{
+        original: "https://i.ibb.co/bmKyH4G/b7dbbc23-2a01-4470-b84a-23efaa84ab32.jpg",
+        thumbnail: "https://i.ibb.co/bmKyH4G/b7dbbc23-2a01-4470-b84a-23efaa84ab32.jpgg",
+        
+},
+{
+        original: "https://i.ibb.co/0rnLsNY/afaf4870-27e1-4e2e-a0d9-32fe4dc13b46.jpg",
+        thumbnail: "https://i.ibb.co/0rnLsNY/afaf4870-27e1-4e2e-a0d9-32fe4dc13b46.jpg",
+        
+},
+
+{
+        original: "https://i.ibb.co/6wx9w2j/9a058e3c-e174-4ac1-958b-732142498c32.jpg",
+        thumbnail: "https://i.ibb.co/6wx9w2j/9a058e3c-e174-4ac1-958b-732142498c32.jpg",
+        
+},
+
+{
+  original: "https://i.ibb.co/z86T839/43319fd3-aac2-442b-9a84-7f34806b5556.jpg",
+  thumbnail: "https://i.ibb.co/z86T839/43319fd3-aac2-442b-9a84-7f34806b5556.jpg",
+  
+},
+{
+  original: "https://i.ibb.co/5hq1RCD/abeba980-47cb-4842-bedf-9c6e8d3453d8.jpg",
+  thumbnail: "https://i.ibb.co/5hq1RCD/abeba980-47cb-4842-bedf-9c6e8d3453d8.jpg",
+  
+},
+{
+  original: "https://i.ibb.co/5hq1RCD/abeba980-47cb-4842-bedf-9c6e8d3453d8.jpg",
+  thumbnail: "https://i.ibb.co/Ky9D0b9/42353387-0c5a-4e08-96eb-55a7eb8845fd.jpg",
+  
+},
+
+
+]
 
 function Nastupi() {
 
-  const setting = {
-    width: '600px',
-    height: ['350px', '200px'],
-    layout: [1, 2],
-    photos: [
-      { src: 'https://i.ibb.co/bmKyH4G/b7dbbc23-2a01-4470-b84a-23efaa84ab32.jpg'},
-      { src: 'https://i.ibb.co/0rnLsNY/afaf4870-27e1-4e2e-a0d9-32fe4dc13b46.jpg'},
-      { src: 'https://i.ibb.co/6wx9w2j/9a058e3c-e174-4ac1-958b-732142498c32.jpg'},
-      { src: 'https://i.ibb.co/z86T839/43319fd3-aac2-442b-9a84-7f34806b5556.jpg'},
-      { src: 'https://i.ibb.co/5hq1RCD/abeba980-47cb-4842-bedf-9c6e8d3453d8.jpg'},
-      { src: 'https://i.ibb.co/Ky9D0b9/42353387-0c5a-4e08-96eb-55a7eb8845fd.jpg'},
-    ],
   
-  showNumOfRemainingPhotos: true
-  };
 
   return (
 
@@ -52,10 +74,13 @@ function Nastupi() {
 
                             
      </div>
-     
-     <div className="collage">
-                              <ReactPhotoCollage {...setting} /> 
-                  </div>
+
+     <div className="gallery">
+
+        <ImageGallery items={images} />;
+     </div>
+        
+
     </div>
   );
 }
