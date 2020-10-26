14a272828d6064adc5f0e179620dcfe7a886c1bb
reah350 <h4riis31@gmail.com>
2020-10-26 16:13:43 +0100

    test12

--- src/Events.js
+++ src/Events.js
@@ -8,62 +8,6 @@ import kolacijada3 from "./images/kolacijada/3.jpg";
 import kolacijada4 from "./images/kolacijada/4.jpg";
 import { Link } from 'react-router-dom';
 
-const setting = {
-  width: '600px',
-  height: ['350px', '200px'],
-  layout: [1, 2],
-  photos: [
-    { src: 'https://i.ibb.co/fSNyXyy/13.jpg'},
-    { src: 'https://i.ibb.co/r0rw1yv/12.jpg'},
-    { src: 'https://i.ibb.co/RN2jcLN/1.jpg'},
-    { src: 'https://i.ibb.co/WtLXVff/11.jpg'},
-    { src: 'https://i.ibb.co/FbCT0Sz/4.jpg'},
-    { src: 'https://i.ibb.co/ByP3nBP/3.jpg'},
-    { src: 'https://i.ibb.co/9Vfpvzz/9.jpg'},
-    { src: 'https://i.ibb.co/T2909Kw/8.jpg'},
-    { src: 'https://i.ibb.co/LnKdBB9/7.jpg'},
-    { src: 'https://i.ibb.co/FbCT0Sz/4.jpg'}
-  ],
-
-showNumOfRemainingPhotos: true
-};
-
-const setting2 = {
-  width: '600px',
-  height: ['350px', '200px'],
-  layout: [2, 3, 1],
-  photos: [
-    { src: 'https://i.ibb.co/6bRn3V2/6.jpg'}, 
-    { src: 'https://i.ibb.co/dWcxwL2/3.jpg'}, 
-    { src: 'https://i.ibb.co/xDfpr1L/4.jpg'}, 
-    { src: 'https://i.ibb.co/hfqQNKQ/5.jpg'}, 
-    { src: 'https://i.ibb.co/k99F8Vn/8.jpg'}, 
-    { src: 'https://i.ibb.co/2q7k9nM/7.jpg'},
-    { src: 'https://i.ibb.co/CzVC7Dt/1.jpg'}, 
-    { src: 'https://i.ibb.co/mvvpM6n/2.jpg'} 
-  ],
-
-showNumOfRemainingPhotos: true
-};
-
-const setting3 = {
-  width: '600px',
-  height: ['350px', '200px'],
-  layout: [1, 2, 1],
-  photos: [
-    { src: 'https://i.ibb.co/Vwf9SH4/1.jpg'}, 
-    { src: 'https://i.ibb.co/hfbHH5v/2.jpg'}, 
-    { src: 'https://i.ibb.co/KNZRKQG/3.jpg'}, 
-    { src: 'https://i.ibb.co/4m5Cqf1/4.jpg'}, 
-    { src: 'https://i.ibb.co/PYBcYts/5.jpg'}, 
-    { src: 'https://i.ibb.co/CmQt5GK/6.jpg'},
-    { src: 'https://i.ibb.co/8748RD5/7.jpg'}, 
-    { src: 'https://i.ibb.co/CPTCwf2/8.jpg'},
-    { src: 'https://i.ibb.co/k4tGKqK/10.jpg'} 
-  ],
-
-showNumOfRemainingPhotos: true
-};
 
 
 function Events() {
@@ -111,10 +55,6 @@ function Events() {
                       
       </div>
 
-    <div className="collage">
-      <ReactPhotoCollage {...setting} /> 
-    </div>
-
     <div class="box-bg">
                    <div class="container"> 
                         <div class="box-a">
@@ -153,9 +93,6 @@ function Events() {
                     </div>
      </div>
 
-    <div className="collage">
-      <ReactPhotoCollage {...setting2} /> 
-    </div>
 
     <div class="box-bg">
                    <div class="container"> 
@@ -173,9 +110,6 @@ function Events() {
                     </div>
      </div>
 
-    <div className="collage">
-      <ReactPhotoCollage {...setting3} /> 
-    </div>
 
     <div class="box-bg">
                    <div class="container"> 
